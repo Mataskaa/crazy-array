a10ed463d7c365723c095e20dd19c33870230767
Mataskaa <mcamataska@gmail.com>
2022-05-10 06:27:51 -0500

    Crazy Array complete - Mataska

--- index.js
+++ index.js
@@ -21,3 +21,31 @@ let crazyArray = [
 // hash in nested array as -> "YEAH" , 12 ,13
 
 //dyanmically, bracket notation, for in, type of, conditions
+
+
+
+for(let i=0; i < crazyArray.length; i++) {
+  if(crazyArray[i] == '#') {
+    crazyArray[i] = 'Hash';
+  }
+  else if(Array.isArray(crazyArray[i]) == true) {
+    for(let j=0; j < crazyArray[i].length; j++) {
+      if(crazyArray[i][j] == '#') {
+        crazyArray[i][j] = 'Yeah';
+      }
+    }
+  }
+  else if(typeof crazyArray[i] == 'object') {
+    for(let k=0; k < crazyArray.length; k++) {
+      for(const key in crazyArray[i]) {
+        if(crazyArray[i][key] == '#') {
+          crazyArray[i][key] = 'Boom';
+        }
+      }
+    }
+  }
+}
+
+
+console.log(crazyArray)
+
