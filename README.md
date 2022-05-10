10bc6695c783dad3f2aa7e06a6e6668d6f1f369b
Mataskaa <mcamataska@gmail.com>
2022-05-10 04:37:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# inserting-into-crazy-array-u5qwt5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/inserting-into-crazy-array-u5qwt5)
---
+++ index.js
@@ -0,0 +1,23 @@
+let crazyArray = [
+  'Elon Musk',
+  'Bootcamp',
+  3,
+  5,
+  { a: 2, b: 8 },
+  '#',
+  { language: 'Japanese', greeting: '#' },
+  767,
+  34,
+  '#',
+  ['#', '#', '#'],
+  ['Tomato', '#', 'PineApple'],
+  { sports: '#' },
+  { students: '#' },
+];
+
+// TODO: Loop through crazy array and replace hash as the following:
+// hash in array as -> "HASH" , 7, 11
+// hash in object as -> "BOOM", 8, 14,15
+// hash in nested array as -> "YEAH" , 12 ,13
+
+//dyanmically, bracket notation, for in, type of, conditions
